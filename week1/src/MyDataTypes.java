9e4da71f5baa87dabc797ac79077beb0c5adcbf1
abdiasisy24 <abdiasisy24@gmail.com>
2025-04-22 10:57:51 -0700

    Add files via upload

---
+++ week1/src/MyDataTypes.java
@@ -0,0 +1,37 @@
+public class MyDataTypes {
+
+    public static void main(String[] arg) {
+
+        short number1 = 32_000;
+        int number2 = 2_000_000;
+        long number3 = 3_000_000_000L;
+        float number4 = 30.57F;
+        double number5 = 50.45637377;
+
+
+        System.out.println(number1);
+        System.out.println(number2);
+        System.out.println(number3);
+        System.out.println(number4);
+        System.out.println(number5);
+
+
+        boolean dry = true;
+
+                if (dry) {
+                    System.out.println("Time to hang up those curtaines!!!");
+                }
+                else {
+                    System.out.println("We will have to wait a spell!");
+                }
+
+        char best = 'A';
+
+        System.out.println(++best);
+
+
+
+
+    }
+
+}
---
+++ week1/src/MyVariable1.java
@@ -0,0 +1,43 @@
+public class MyVariable1 {
+
+    public static void main(String[] args) {
+
+        String name = "Abdiasis";
+//        After a statement, Don't forget the semi colon!
+        int hours = 40;
+//        We are assingning 40 to the hours variable
+//        Please note, When we have a string value, It is nested in "".
+//        When we are referring to numbers/integers, No quotes!!!
+        System.out.println(name);
+        System.out.println(hours);
+
+        String building, tree, lake, ocean;
+        tree = "Redwood";
+        building = "Empire state";
+        lake = "Lake superior";
+        ocean = "Pacific ocean";
+
+//        String building = "Empire stete", tree = "Maple", lake = "lake superior", ocean = "pacific ocean";
+
+        int weeks, decades, months, years;
+        weeks = 40;
+        decades = 10;
+        months = 3;
+        years = 5;
+
+//        int weeks = 4, decades = 10, months = 3, years = 5;
+
+        System.out.println(tree);
+        System.out.println(building);
+        System.out.println(lake);
+        System.out.println(ocean);
+        System.out.println(weeks);
+        System.out.println(decades);
+        System.out.println(months);
+        System.out.println(years);
+
+
+
+    }
+
+}
